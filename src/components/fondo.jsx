09130e3b6faa518e8c0c481481f2f94826962a2b
Gonzalo Luna <lunagonzalo240mail.com>
2023-10-23 15:25:06 -0500

    mejora del fondo y el logo

--- src/components/fondo.jsx
+++ src/components/fondo.jsx
@@ -12,56 +12,19 @@ export function Fondo() {
             <div className="f-item f2">
             </div>
 
+            <div className="f-item f4">
+            </div>
+
+
+            <div className="f-item f5">
+            </div>
+
             <div className="f-item f3">
             </div>
 
-{/*             <div className="f-item f4">
-            </div> */}
-
-{/*             <div className="f-item f5">
-            </div>
-
             <div className="f-item f6">
-            </div> */}
-
-
-{/*             <div className="f-item f7">
             </div>
 
-            <div className="f-item f8">
-            </div>
-
-            <div className="f-item f9">
-            </div>
-
-            <div className="f-item f10">
-            </div> */}
-
-{/*             <div className="f-item f11">
-            </div>
-
-            <div className="f-item f12">
-            </div>
- */}
-            
-
-{/*             <div className="f-item f13">
-            </div>
-
-            <div className="f-item f14">
-            </div>
-
-            <div className="f-item f15">
-            </div>
-
-            <div className="f-item f16">
-            </div> */}
-
-{/*             <div className="f-item f17">
-            </div>
-
-            <div className="f-item f18">
-            </div> */}
         </motion.div>
     )
 }
